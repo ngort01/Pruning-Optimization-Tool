b2bf5dbbcff135c8a08543b999797ba2d64aecb9
ngort01 <n.gorte@gmx.de>
2015-12-13 20:41:27 +0100

    jar

--- src/de/ifgi/optimizer/Optimizer.java
+++ src/de/ifgi/optimizer/Optimizer.java
@@ -1,9 +1,15 @@
 package de.ifgi.optimizer;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.jgrapht.alg.ConnectivityInspector;
@@ -20,38 +26,40 @@ public class Optimizer {
 
 	}
 
-	public void optimize(ParsedInput input) {
+	public void optimize(ParsedInput input, String outputFile) throws IOException {
 		SimpleWeightedGraph<Geometry, Relation> g = input.getG();
 		ConnectivityInspector<Geometry, Relation> inspector = new ConnectivityInspector<Geometry, Relation>(g);
 		// grounded points that were already printed (to prevent doubled output)
 		ArrayList<Geometry> printed = new ArrayList<Geometry>();
-		
+
 		// cut dc and ntpp relations before graph decomposition
 		g.edgeSet().iterator().forEachRemaining(e -> {
-				if (e.getType().equalsIgnoreCase("dc") | e.getType().equalsIgnoreCase("ntpp")) g.removeEdge(e);
-			}
-		);
-		
-		int[] xRange = {-2, -1};
+			if (e.getType().equalsIgnoreCase("dc") | e.getType().equalsIgnoreCase("ntpp"))
+				g.removeEdge(e);
+		});
+
+		int[] xRange = { -2, -1 };
 		inspector.connectedSets().forEach(set -> {
 			xRange[0] += 2;
 			xRange[1] += 2;
 			Subgraph subGraph = new Subgraph(g, set);
 			System.out.println(subGraph);
-			// choose points for grounding depending on vertex score and relation type
+			// choose points for grounding depending on vertex score and
+			// relation type
 			ArrayList<Geometry> chosenObjects = chooseObjects(g, subGraph);
 			// apply case F
 			caseF(chosenObjects, xRange[0], xRange[1]);
 		});
 
-		
 		// Output
+		List<String> lines = new ArrayList<String>();
+		lines.add("## OUTPUT");
 		System.out.println("## OUTPUT");
 		g.edgeSet().iterator().forEachRemaining(e -> {
 			Geometry g1 = (Geometry) e.getV1();
 			Geometry g2 = (Geometry) e.getV2();
-			// if there is a centre relation between two objects 
-			// and one is grounded, ground the other one 
+			// if there is a centre relation between two objects
+			// and one is grounded, ground the other one
 			String type = e.getType();
 			if (type.contentEquals("centre") | type.contentEquals("equal")) {
 				if (g1.isGrounded()) {
@@ -62,20 +70,32 @@ public class Optimizer {
 					g1.setY(g2.getY());
 				}
 			}
-			
-			if(!printed.contains(g1)) {
+
+			if (!printed.contains(g1)) {
 				g1.print();
+				if (g1.isGrounded()) {
+					lines.add(g1.getName() + "x = " + g1.getX());
+					lines.add(g1.getName() + "x = " + g1.getY());
+				}
 				printed.add(g1);
 			}
-			if(!printed.contains(g2)) {
+			if (!printed.contains(g2)) {
 				g2.print();
+				if (g1.isGrounded()) {
+					lines.add(g2.getName() + "x = " + g2.getX());
+					lines.add(g2.getName() + "x = " + g2.getY());
+				}
 				printed.add(g2);
 			}
 		});
+
+		Path file = Paths.get(outputFile);
+		Files.write(file, lines, Charset.forName("UTF-8"));
 	}
-	
+
 	/**
 	 * Choose points for grounding depending on the node score and relations
+	 * 
 	 * @param vertices
 	 * @param chosenObjects
 	 * @param g
@@ -87,11 +107,11 @@ public class Optimizer {
 		subGraph.vertexSet().toArray(vertexSet);
 		Arrays.sort(vertexSet);
 
-		
 		for (int i = 0; i < vertexSet.length; i++) {
 			Geometry v = vertexSet[i];
-			if (chosenObjects.size() > 1) break;
-			
+			if (chosenObjects.size() > 1)
+				break;
+
 			if (i == 0) {
 				// add vertex with highest score
 				chosenObjects.add(v);
@@ -100,22 +120,24 @@ public class Optimizer {
 				for (int j = 0; j < size; j++) {
 					if (!g.containsEdge(chosenObjects.get(j), v)) {
 						chosenObjects.add(v);
-						 break;
+						break;
 					} else {
 						String type = g.getEdge(chosenObjects.get(j), v).getType();
-						if(!type.equalsIgnoreCase("centre") && !type.equalsIgnoreCase("equal"))  chosenObjects.add(v);
+						if (!type.equalsIgnoreCase("centre") && !type.equalsIgnoreCase("equal"))
+							chosenObjects.add(v);
 					}
 				}
-				
+
 			}
-			
+
 		}
-		
+
 		return chosenObjects;
 	}
-	
+
 	/**
 	 * Grounds two objects
+	 * 
 	 * @param chosenObjects
 	 */
 	private void caseF(ArrayList<Geometry> chosenObjects, int minX, int maxX) {
--- src/main.java
+++ src/main.java
@@ -1,16 +1,23 @@
+import java.io.IOException;
+
 import de.ifgi.importer.FileReader;
 import de.ifgi.importer.ParsedInput;
 import de.ifgi.optimizer.Optimizer;
 
 public class main {
-
-	public static void main(String[] args) {
+	/**
+	 * 
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
 		FileReader reader = new FileReader();
 		Optimizer optimizer = new Optimizer();
-		ParsedInput input = reader.readFile("C:/Users/Niko/Desktop/graph_decomposition_example_2.txt");
+		//ParsedInput input = reader.readFile("C:/Users/Niko/Desktop/graph_decomposition_example_2.txt");
+		ParsedInput input = reader.readFile(args[0]);
 		input.calcScores();
 		//input.print();
-		optimizer.optimize(input);
+		optimizer.optimize(input, args[1]);
 	}
 
 }
